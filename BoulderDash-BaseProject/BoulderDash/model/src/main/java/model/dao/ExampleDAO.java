aae704fef6b35d43a34d9a456f03e71e08a829b3
BornToWard <julien.his@viacesi.fr>
2017-06-17 17:53:06 +0000

    Data receiver

--- BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/dao/ExampleDAO.java
+++ BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/dao/ExampleDAO.java
@@ -1,101 +1,130 @@
 package model.dao;
 
-import java.sql.CallableStatement;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.sql.Blob;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
-import model.Example;
 
-/**
- * <h1>The Class ExampleDAO.</h1>
- *
- * @author Jean-Aymeric DIET jadiet@cesi.fr
- * @version 1.0
- */
-public abstract class ExampleDAO extends AbstractDAO {
+public class ExampleDAO {
+	
 
-    /** The sql example by id. */
-    private static String sqlExampleById   = "{call findExampleById(?)}";
+	// to test : public static void main(final String argv[]) throws SQLException, IOException {
+ 	public static void getMap(int idLvl) throws SQLException, IOException{
+ 	// to test : int idLvl = "what you want to force to start"
+    PrintWriter writer = null;
+	try {
+		writer = new PrintWriter(new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream("RECEIVED.txt"))));
+	} catch (FileNotFoundException e1) {
+		e1.printStackTrace();
+	}
+    BoulderDashBDDConnector co = new BoulderDashBDDConnector();
 
-    /** The sql example by name. */
-    private static String sqlExampleByName = "{call findExampleByName(?)}";
+    
 
-    /** The sql all examples. */
-    private static String sqlAllExamples   = "{call findAllExamples()}";
+    co.open();
+    
+    co.executeQuery("SELECT * FROM example");
+    try (ResultSet resultSet = co.executeQuery("SELECT name FROM example WHERE id =" + idLvl)) {
 
-    /** The id column index. */
-    private static int    idColumnIndex    = 1;
-
-    /** The name column index. */
-    private static int    nameColumnIndex  = 2;
-
-    /**
-     * Gets the example by id.
-     *
-     * @param id
-     *            the id
-     * @return the example by id
-     * @throws SQLException
-     *             the SQL exception
-     */
-    public static Example getExampleById(final int id) throws SQLException {
-        final CallableStatement callStatement = prepareCall(sqlExampleById);
-        Example example = null;
-        callStatement.setInt(1, id);
-        if (callStatement.execute()) {
-            final ResultSet result = callStatement.getResultSet();
-            if (result.first()) {
-                example = new Example(result.getInt(idColumnIndex), result.getString(nameColumnIndex));
-            }
-            result.close();
-        }
-        return example;
-    }
-
-    /**
-     * Gets the example by name.
-     *
-     * @param name
-     *            the name
-     * @return the example by name
-     * @throws SQLException
-     *             the SQL exception
-     */
-    public static Example getExampleByName(final String name) throws SQLException {
-        final CallableStatement callStatement = prepareCall(sqlExampleByName);
-        Example example = null;
-
-        callStatement.setString(1, name);
-        if (callStatement.execute()) {
-            final ResultSet result = callStatement.getResultSet();
-            if (result.first()) {
-                example = new Example(result.getInt(idColumnIndex), result.getString(nameColumnIndex));
-            }
-            result.close();
-        }
-        return example;
-    }
-
-    /**
-     * Gets the all examples.
-     *
-     * @return the all examples
-     * @throws SQLException
-     *             the SQL exception
-     */
-    public static List<Example> getAllExamples() throws SQLException {
-        final ArrayList<Example> examples = new ArrayList<Example>();
-        final CallableStatement callStatement = prepareCall(sqlAllExamples);
-        if (callStatement.execute()) {
-            final ResultSet result = callStatement.getResultSet();
-
-            for (boolean isResultLeft = result.first(); isResultLeft; isResultLeft = result.next()) {
-                examples.add(new Example(result.getInt(idColumnIndex), result.getString(nameColumnIndex)));
-            }
-            result.close();
-        }
-        return examples;
-    }
+    	while(resultSet.next()) { // for each row
+ 		   // take the blob
+ 		   Blob blob = resultSet.getBlob("name");
+ 		   BufferedInputStream is = new BufferedInputStream(blob.getBinaryStream());
+ 		   FileOutputStream fos = new FileOutputStream("RECEIVED.txt");
+ 		   // you can set the size of the buffer
+ 		   byte[] buffer = new byte[2048];
+ 		   int r = 0;
+ 		   while((r = is.read(buffer))!=-1) {
+ 		      fos.write(buffer, 0, r);
+ 		   }
+ 		   fos.flush();
+ 		   fos.close();
+ 		   is.close();
+ 		   blob.free();
+ 		}
+ 	resultSet.close();
+		writer.flush();
+		writer.close();
+	}
+ 
+ decrypt();
+ // use this to generate the map : generatelvl();   
+    
 }
+  
+public static void decrypt(){
+		
+	    ObjectInputStream ois = null;
+	     
+
+	    try {
+	      final FileInputStream fichier = new FileInputStream("RECEIVED.txt");
+	      ois = new ObjectInputStream(fichier);
+	      final Map map1 = (Map) ois.readObject();
+	      
+	      System.out.println("Lvl name = " + map1.getName());
+	      System.out.println("Id = " + map1.getId());
+	      System.out.println("Nbdiamond = " + map1.getNbdiamond());
+	      
+	      System.out.println("width = " + map1.getWidth());
+	      System.out.println("height = " + map1.getHeight());
+	      System.out.println("code map = " + map1.getCodemap());
+	    } catch (final java.io.IOException e) {
+	      e.printStackTrace();
+	    } catch (final ClassNotFoundException e) {
+	      e.printStackTrace();
+	    } finally {
+	      try {
+	        if (ois != null) {
+	          ois.close();
+	        }
+	      } catch (final IOException ex) {
+	        ex.printStackTrace();
+	      }
+	    }
+	  }
+	
+
+
+	public static void generatelvl(){
+		
+		
+		
+	    final Map map1 = new Map("LELELE", 10, 18, 12, "REOOOOOOOOOOOOOOORPREOOOOOOOOOOOOORSDOREOOOOOOOOOOORODRDOREOOOOOOOOORODRORDOREOOOOOOORODROOORDOREOOOOORODROOOOORDOREOOORODROOOOOOORDOREORODROOOOOOOOORDORRODROOOOOOOOOOORDOOOROOOOOO", 1);
+	    ObjectOutputStream oos = null;
+	    PrintWriter writer = null;
+		try {
+			writer = new PrintWriter(new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream("RECEIVED.txt"))));
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+		}
+	    try {
+	      final FileOutputStream fichier = new FileOutputStream("donnees.txt");
+	      oos = new ObjectOutputStream(fichier);
+	      oos.writeObject(map1);
+	      oos.flush();
+	    } catch (final java.io.IOException e) {
+	      e.printStackTrace();
+	    } finally {
+	      try {
+	        if (oos != null) {
+	          oos.flush();
+	          oos.close();
+	        }
+	      } catch (final IOException ex) {
+	        ex.printStackTrace();
+	      }
+	    }
+	}
+}
+
