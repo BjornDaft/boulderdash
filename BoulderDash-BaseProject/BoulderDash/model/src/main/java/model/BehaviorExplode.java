32803bbba4f484d1d80d2c6e63534ee90b1abb9b
Jersey27 <tristan.lefevre@viacesi.fr>
2017-06-20 14:06:48 +0200

    add BehaviorExplode
    -add BehaviorExplode, for enemy. If an player is near monster (or a
    rock/diamond at his head), it will explode.
    -add method test

--- BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/BehaviorExplode.java
+++ BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/BehaviorExplode.java
@@ -1,7 +1,22 @@
 package model;
 
+
+
 public class BehaviorExplode extends Behavior {
+	int i, j;
+	boolean alive = false;
 	public void behavior() {
+		if (tab[posX--][posY] ='P' || tab[posX ++][posY] =='P' || tab[posX][posY ++] =='P' || tab[posX][posY --] =='P' || tab[posX][posY --] =='R' || tab[posX][posY--] =='D')
+		 {
+		 	for(i = posX --;i = posX ++; i++)
+		 		{
+		 		for(tposY = posY --;tposY =posY ++; tposY ++)
+		 			{
+		 			tab[i][j] = "X";
+		 			}
+		 		}
+		 	Ennemy.setIsAlive(alive);
+		 }
 		
 	}
 }
--- BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/BehaviorReward.java
+++ BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/BehaviorReward.java
@@ -1,8 +1,20 @@
 package model;
 
 public class BehaviorReward extends Behavior {
+	int tposX, tposY;
 	public void behavior() 
 	{
+		if (tab[posX--][posY] ="P" || tab[posX ++][posY] =="P" || tab[posX][posY ++] =="P" || tab[posX][posY --] =="P" || tab[posX][posY --] =="R")
+		 {
+		 	for(tposX = posX --,tposX =posX ++, tposX ++)
+		 		{
+		 		for(tposY = posY --,tposY =posY ++, tposY ++)
+		 			{
+		 			tab[tposX][tposY] = "D";
+		 			new Diamond(position);
+		 			}
+		 		}
+		 }
 		
 	}
 }
