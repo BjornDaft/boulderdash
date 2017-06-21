6844c7463dc8e9a649cbc4fe7594e6398a162818
BornToWard <julien.his@viacesi.fr>
2017-06-21 12:23:52 +0000

    DiamondTest.java

---
+++ BoulderDash-BaseProject/BoulderDash/model/model-test/model/DiamondTest.java
@@ -0,0 +1,13 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DiamondTest {
+
+	@Test
+	public void testBehavior() {
+		// Diamond has to be done !
+	}
+}
