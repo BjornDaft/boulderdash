bd59fb27e9ea02ec5629dcebdd193810334afc46
Jersey27 <tristan.lefevre@viacesi.fr>
2017-06-21 10:00:16 +0200

    Modify gravity (add method)
    -add method in gravity for moving rock/diamond depending of the
    situation (reduce code in behavior method and add new method for each
    possibilities)

--- BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/behavior/BehaviorGravity.java
+++ BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/behavior/BehaviorGravity.java
@@ -1,55 +1,70 @@
-package model;
+package model.behavior;
 
 import java.util.Random;
 
+import model.Mobile;
+import model.Position;
 import model.dao.ExampleDAO;
 
 public class BehaviorGravity extends Behavior {
-	
-	public void behavior(Position position)
+	private int posX;
+	private int posY;
+	public void behavior()
 	{
-	while(this.array.getChar(posX, posY++) == 'X' || this.array.getChar(posX, posY++) == 'R' || this.array.getChar(posX, posY++) == 'D')  
+	while(this.array.getChar(posX, posY) == 'X' || this.array.getChar(posX, posY++) == 'R' || this.array.getChar(posX, posY++) == 'D')  
 		{
 		if(this.array.getChar(posX, posY++) == 'X')
-			{
-			this.array.setChar(posX, posY++, 'R');
-			this.array.setChar(posX, posY, 'X');
-			position.setY(posY++);
+		{
+			this.fallDown();
 			}
 		if (this.array.getChar(posX, posY++) =='R' || this.array.getChar(posX, posY++) =='D')
 			{
-			if(this.array.getChar(posX++, posY++) == 'X' && this.array.getChar(posX++, posY)=='X')
-				{
-				this.array.setChar(posX++, posY, 'R');
-				this.array.setChar(posX, posY, 'X');
-				posX++;
-				}
-			else if(this.array.getChar(posX--, posY++)=='X' && this.array.getChar(posX++, posY)=='X')
-				{
-				this.array.setChar(posX, posY++, 'R');
-				this.array.setChar(posX++, posY++, 'X');
-				posX--;
-				}
-			else if(this.array.getChar(posX--, posY++)=='X' && this.array.getChar(posX--, posY)=='X' && this.array.getChar(posX++, posY++) =='X' && this.array.getChar(posX++, posY)=='X')
-				{
-				Random rand =new Random();
-				boolean gauche = rand.nextBoolean();
-				if (gauche = true) {
-					this.array.setChar(posX++, posY, 'R');
-					this.array.setChar(posX, posY, 'X');
-					posX++;
-				}
-				else {
-					this.array.setChar(posX, posY++, 'R');
-					this.array.setChar(posX++, posY++, 'X');
-					posX--;
-				}
-				}
-			else if(this.array.getChar(posX--, posY) != 'X' && this.array.getChar(posX++, posY)!='X')
-				{
-				break;
-				}
+			this.VerticalFall();
 			}
+		}
+	}
+	public void VerticalFall() {
+		if(this.array.getChar(posX++, posY++) == 'X' && this.array.getChar(posX++, posY)=='X')
+		{
+			this.fallRight();
+		}
+	else if(this.array.getChar(posX--, posY++)=='X' && this.array.getChar(posX++, posY)=='X')
+		{
+			this.fallLeft();
+		}
+	else if(this.array.getChar(posX--, posY++)=='X' && this.array.getChar(posX--, posY)=='X' && this.array.getChar(posX++, posY++) =='X' && this.array.getChar(posX++, posY)=='X')
+		{
+			this.randomFall();
+		}
+	else if(this.array.getChar(posX--, posY) != 'X' && this.array.getChar(posX++, posY)!='X')
+		{
+		return;
+		}
+	}
+	public void fallDown() {
+
+		this.array.setChar(posX, posY++, 'R');
+		this.array.setChar(posX, posY, 'X');
+		Position.setY(posY++);
+	}
+	public void fallLeft() {
+		this.array.setChar(posX--, posY, 'R');
+		this.array.setChar(posX, posY, 'X');
+		Position.setX(posX--);
+	}
+	public void fallRight() {
+		this.array.setChar(posX++, posY, 'R');
+		this.array.setChar(posX, posY, 'X');
+		Position.setX(posX++);
+	}
+	public void randomFall() {
+		Random rand =new Random();
+		boolean gauche = rand.nextBoolean();
+		if (gauche == true) {
+			this.fallLeft();
+		}
+		else {
+			this.fallRight();
 		}
 	}
 }
