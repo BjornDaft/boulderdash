652a0e597595de44b5cb23b0d7e80a827e09265f
BornToWard <julien.his@viacesi.fr>
2017-06-21 11:25:06 +0000

    ArrayTest.java

--- BoulderDash-BaseProject/BoulderDash/model/model-test/model/ArrayTest.java
+++ BoulderDash-BaseProject/BoulderDash/model/model-test/model/ArrayTest.java
@@ -10,27 +10,16 @@ public class ArrayTest {
 
 	@Test
 	public void testGetChar() {
-		try {
-			char[][] tab = new char[5][5];
-			tab[0][0] = 'Z';
-			fail("Should throw an exception if the char isn't recognized");
-		} catch (final Exception e) {
-			final String expected = "getChar isnt returning the right value, the tab is corrupted";
-			assertEquals(expected, e.getMessage());
-		}
+		final char expected = 'A';
+		this.array.setChar(0, 0, 'A');
+		assertEquals(expected, this.array.getChar(0, 0));
 
 	}
 
 	@Test
 	public void testSetChar() {
-		try {
-			char[][] tab = new char[5][5];
-			tab[0][0] = 'N';
-			fail("Should throw an exception if the char input isnt an allowed one");
-		} catch (final Exception e) {
-			final String expected = "You can't set this char";
-			assertEquals(expected, e.getMessage());
-
+		final char expected = 'A';
+		this.array.setChar(0, 0, 'A');
+		assertEquals(expected, this.array.getChar(0, 0));
 		}
-	}
 }
