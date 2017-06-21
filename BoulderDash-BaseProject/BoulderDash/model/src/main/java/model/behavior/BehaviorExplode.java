082c214970595de944ece5959d685cc02d1981dd
BornToWard <julien.his@viacesi.fr>
2017-06-21 13:24:59 +0000

    fixed 'package' on few classes

--- BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/behavior/BehaviorExplode.java
+++ BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/behavior/BehaviorExplode.java
@@ -1,4 +1,4 @@
-package model;
+package model.behavior;
 
 
 public class BehaviorExplode extends Behavior {
--- BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/behavior/BehaviorPlayerAction.java
+++ BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/behavior/BehaviorPlayerAction.java
@@ -1,4 +1,4 @@
-package model;
+package model.behavior;
 
 public class BehaviorPlayerAction extends Behavior{
 	public void behavior() 
