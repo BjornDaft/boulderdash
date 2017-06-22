bb702a12965903ed1f0640c2cc9cb6aaff1c31b6
BornToWard <julien.his@viacesi.fr>
2017-06-22 10:15:11 +0000

    Enemy AI

--- BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/Enemy.java
+++ BoulderDash-BaseProject/BoulderDash/model/src/main/java/model/Enemy.java
@@ -5,15 +5,55 @@ package model;
 public class Enemy extends Element implements IMove {
 	private static int IMAGEPOSITIONX = 16;
 	private static int IMAGEPOSITIONY = 1;
+	private Position position;
 	
-	public Enemy(Position position, final int imagePositionX, final int imagePositionY) {
+	public Enemy(Position position) {
 		super(position, IMAGEPOSITIONX, IMAGEPOSITIONY);
-
+		this.position = position;
 	}
 
 	@Override
 	public void move() {
-		// TODO Auto-generated method stub
+
+		int x = position.getX();
+		int y = position.getY();
+		Boolean previousDirectionX;
+		Boolean previousDirectionY;
 		
+		if(array.getType(x, y++) != "Avoid"){
+			x++;
+			previousDirectionX = true; //wasgoingtoright
+			position.setX(x);
+		}
+		else if(array.getType(x++, y) != "Avoid"){
+			y--;
+			previousDirectionY = false; //wasgoingdown
+			position.setY(y);
+		}
+		else if(array.getType(x, y--) != "Avoid"){
+			x--;
+			previousDirectionX = false; //wasgoingtoleft
+			position.setX(x);
+		}
+		else if(array.getType(x--, y) != "Avoid"){
+			y++;
+			previousDirectionY = true; //wasgoingup
+			position.setY(y);
+		}
+		if(array.getType(x, y++) == "Avoid" && array.getType(x, y--) == "Avoid"){
+			if(previousDirectionX = true)
+				y++;
+			else
+				y--;
+			position.setY(y);
+			
 	}
+		if(array.getType(x++, y) == "Avoid" && array.getType(x--, y) == "Avoid"){
+			if(previousDirectionY = true)
+				x--;
+			else
+				x++;
+			position.setX(y);
+		}
  }
+}
