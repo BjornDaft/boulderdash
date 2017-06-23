dc601e3e04761b15ba7ea0e3980ee706fdcc2954
BjornDaft <romain.jouatte@viacesi.fr>
2017-06-23 11:33:18 +0200

    Try to make interface to tranfers information

---
+++ BoulderDash-BaseProject/BoulderDash/contract/src/main/java/model/IElement.java
@@ -0,0 +1,9 @@
+package model;
+
+import java.awt.image.BufferedImage;
+
+public interface IElement {
+	public IPosition getPosition();
+	public BufferedImage getImage();
+	public IModelFacade getModelFacade() ;
+}
