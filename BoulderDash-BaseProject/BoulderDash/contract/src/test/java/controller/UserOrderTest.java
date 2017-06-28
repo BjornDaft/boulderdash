0c9f66ce875998700f4bcdc025b63ec08732f4c3
BornToWard <julien.his@viacesi.fr>
2017-06-28 16:11:03 +0000

    UserOrderTest update

--- BoulderDash-BaseProject/BoulderDash/contract/src/test/java/controller/UserOrderTest.java
+++ BoulderDash-BaseProject/BoulderDash/contract/src/test/java/controller/UserOrderTest.java
@@ -1,14 +1,19 @@
 package controller;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
 
-import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 
-@SuppressWarnings("unused")
 public class UserOrderTest {
 	
-	private Order order;
+	@Before
+	public void setup(){
+		this.order = new UserOrder(Order.UP);
+	}
+	
+	private UserOrder order;
 	
 	@Test
 	public void testGetOrder() {
@@ -16,7 +21,7 @@ public class UserOrderTest {
 		try{
 	        assertNotNull(this.order);//check if the object is != null
 	        //checks if the returned object is of class Expression
-	        assertTrue(this.order instanceof Order);
+	        equals(order.getOrder() instanceof Order);
 	        
 	    }catch(Exception e){
 	        // let the test fail, if your function throws an Exception.
